b0eead6fe6c52472deeab3a2a7e06b14ca188af9
faizFe <fhanafi109@gmail.com>
2026-04-28 08:56:10 +0800

    Refactor LoginPage UI and implement animation; update main app entry point to use SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-// import 'package:banuainsight_project/features/login/splash_screen.dart';
-// import 'package:banuainsight_project/features/profile/profile.dart';
-import 'package:banuainsight_project/features/news_detail/ui/detail_berita.dart';
+import 'package:banuainsight_project/features/login/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       // Kita ubah *home* nya dari SplashScreen menjadi DetailBerita agar langsung muncul saat di-run
-      home: const DetailBerita(), 
+      home: const SplashScreen(), 
     );
   }
 }
